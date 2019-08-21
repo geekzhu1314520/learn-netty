939913c6fc1d525aac4ea39239f68a3a653cf4ca
zhujiashuai <jiashuai.zhu@ishansong.com>
2019-08-21 14:05:04 +0800

    pipeline与channelHandler

--- src/main/java/com/watermelon/netty/server/handler/inbound/InBoundHandlerC.java
+++ src/main/java/com/watermelon/netty/server/handler/inbound/InBoundHandlerC.java
@@ -7,6 +7,6 @@ public class InBoundHandlerC extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("InBoundHandlerC:" + msg);
-        super.channelRead(ctx, msg);
+        ctx.channel().writeAndFlush(msg);
     }
 }
