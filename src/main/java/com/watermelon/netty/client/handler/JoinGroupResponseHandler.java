e48f580fb2388d8e8b7975f9f28716b570130348
zhujiashuai <jiashuai.zhu@ishansong.com>
2019-09-27 11:36:09 +0800

    format code

--- src/main/java/com/watermelon/netty/client/handler/JoinGroupResponseHandler.java
+++ src/main/java/com/watermelon/netty/client/handler/JoinGroupResponseHandler.java
@@ -5,7 +5,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 public class JoinGroupResponseHandler extends SimpleChannelInboundHandler<JoinGroupResponsePacket> {
-
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, JoinGroupResponsePacket msg) throws Exception {
         String groupId = msg.getGroupId();
